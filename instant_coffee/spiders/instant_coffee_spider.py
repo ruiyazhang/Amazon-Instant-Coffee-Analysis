2d49c384f4be52c823cb502ecf57ccdd3dddeb7e
ruiyazhang <zhangruiya@gmail.com>
2020-07-23 01:04:38 -0400

    more product details added

--- instant_coffee/spiders/instant_coffee_spider.py
+++ instant_coffee/spiders/instant_coffee_spider.py
@@ -14,34 +14,105 @@ class InstantCoffeeSpider(Spider):
 
         # print(len(url_list))
 
-        for url in url_list[:1]:
+        for url in url_list[:2]:
             yield Request(url=url, callback=self.parse_results_page)
     
     def parse_results_page(self, response):
         product_image_urls = response.xpath('//div[@class="s-main-slot s-result-list s-search-results sg-row"]/div[not(contains(@class, "AdHolder"))]//div[@class="a-section aok-relative s-image-square-aspect"]/img/@src').extract()
         product_name = response.xpath('//div[@class="s-main-slot s-result-list s-search-results sg-row"]/div[not(contains(@class, "AdHolder"))]//h2/a/span/text()').extract()
         product_urls = response.xpath('//div[@class="s-main-slot s-result-list s-search-results sg-row"]/div[not(contains(@class, "AdHolder"))]//h2/a/@href').extract()
+
+        rating_avgs = response.xpath('//div[@class="s-main-slot s-result-list s-search-results sg-row"]/div[not(contains(@class, "AdHolder"))]//div[@class="s-expand-height s-include-content-margin s-border-bottom s-latency-cf-section"]/div[1]/div[@class="a-section a-spacing-none a-spacing-top-micro"]/div[1]/span[1]/@aria-label').extract()
+        rating_nums = response.xpath('//div[@class="s-main-slot s-result-list s-search-results sg-row"]/div[not(contains(@class, "AdHolder"))]//div[@class="s-expand-height s-include-content-margin s-border-bottom s-latency-cf-section"]/div[1]/div[@class="a-section a-spacing-none a-spacing-top-micro"]/div[1]/span[2]/@aria-label').extract()
+
         # print('='*50)
         # print(len(product_urls))
-        # print(len(product_name))
+        # print(rating_avgs)
+        # print(rating_nums)
         # print(len(product_image_urls))
         # print('='*50)
-        product_urls = [f'https://www.amazon.com{url}' for url in product_urls]
-        for url in product_urls[:8]:
-            yield Request(url=url, callback=self.parse_product_page)
+
+        product_urls = [f'https://www.amazon.com{url}&psc=1' for url in product_urls]
+        # for url in product_urls[:10]:
+        for index in range(5):
+            url = product_urls[index]
+
+            meta = {
+                'product_image_url': product_image_urls[index],
+                'product_name': product_name[index],
+                'rating_avg': rating_avgs[index],
+                'rating_num': rating_nums[index]
+            }
+    
+            yield Request(url=url, callback=self.parse_product_page, meta=meta)
 
     def parse_product_page(self, response):
-        brand = response.xpath('//div[@id="bylineInfo_feature_div"]/div/a/text()').extract_first()
-        price = response.xpath('//div[@id="price"]//span[@id="priceblock_ourprice"]/text()').extract_first() 
+        
+        index_of_dp=response.url.find('/dp/')
+        url_1 = response.url[index_of_dp+4:]
+        index_of_ref =url_1.find('/ref=')
+        ASIN = url_1[:index_of_ref]
 
+        brand = response.xpath('//div[@id="bylineInfo_feature_div"]/div/a/text()').extract_first()
+        
+        if not brand:
+            brand=response.xpath('//div[@id="bylineInfoUS_feature_div"]/div[@class="a-section a-spacing-none"]/text()').extract_first()
+            brand=str.strip(brand)
+            # brand=(re.findall("^.{1,7}(.*)$",brand))[0]
+
+        price_first_try= response.xpath('//div[@id="price"]//span[@id="priceblock_ourprice"]/text()').extract_first()
+        price_second_try= response.xpath('//tr[@id="almDetailPagePrice_buying_price"]//span[@id="priceblock_ourprice"]/text()').extract_first()
+        price_third_try = response.xpath('//span[@class="a-color-price"]/text()').extract_first()
+        price = price_first_try or price_second_try or price_third_try
+                
         choice = response.xpath('//div[@id="acBadge_feature_div"]/div[1]//span[@class="a-size-small aok-float-left ac-badge-rectangle"]/span/text()').extract()
         l = ''
         for a in choice:
             l += a
         amazon_choice=(l=='Amazon\'s Choice')
-        # print('='*50)
-        # print(brand)
-        # print(price)
-        # print(amazon_choice)
-        # print('='*50)
+
+        unit_price_first_try = response.xpath('//div[@id="unifiedPrice_feature_div"]//tr[@id="priceblock_ourprice_row"]//span[@class="a-size-small a-color-price"]/text()').extract_first()
+        unit_price_second_try = response.xpath('//tr[@id="almDetailPagePrice_buying_price"]//tr[@id="priceblock_ourprice_row"]//td[@class="a-span12"]/span[@class="a-size-small a-color-price"]/text()').extract_first()
+        unit_price_third_try = response.xpath('//div[@id="detail-bullets"]/table//div[@class="content"]/ul/li[1]/text()').extract_first()
+        unit_price_fourth_try = response.xpath('//table[@id="productDetails_detailBullets_sections1"]//tr[2]/td[@class="a-size-base"]/text()').extract_first()
+        unit_price_fifth_try = response.xpath('//div[@id="detail-bullets"]/table//div[@class="disclaim"]/strong[last()]/text()').extract_first()
+
+        unit_price = unit_price_first_try or unit_price_second_try or unit_price_third_try or unit_price_fourth_try or unit_price_fifth_try
+
+        try:
+            unit_price = str.strip(unit_price)
+        except:
+            None
+
+        top_sales_rank = response.xpath('//li[@class="zg_hrsr_item"]/span[@class="zg_hrsr_rank"]/text()').extract_first()
+        top_sales_rank = top_sales_rank or response.xpath('//table[@id="productDetails_detailBullets_sections1"]//tr[9]/td/span/span[2]/text()').extract_first()
+        
+        try:
+            top_sales_rank = int((re.findall('#(\d+)',top_sales_rank))[0])
+        except:
+            None
+
+        rating_avg = None
+        try:
+            rating_avg = float((re.findall('\d+\.\d+',response.meta['rating_avg']))[0])
+        except:
+            None
+
+        rating_num = None
+        try:
+            rating_num = response.meta['rating_num']
+        except:
+            None
+
+        print('='*50)
+        print(brand)
+        print(price)
+        print(amazon_choice)
+        print(unit_price)
+        print(ASIN)
+        print(top_sales_rank)
+        print(response.meta['product_name'])
+        print(rating_avg)
+        print(rating_num)
+        print('='*50)
         
